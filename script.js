620c2f4e55ae2a9fead05e5b5a187a01912eb72b
Neil Gandhi <65824883+ntch2000@users.noreply.github.com>
2020-10-07 15:37:42 -0400

    Created initial api call for sample city weather

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
-console.log("Hello");
+$(document).ready(function () {
+  console.log("Hello");
+
+  // DOM VARIABLES
+
+  // JS VARIABLES
+
+  var apiKey = "a804367293883745a69dec50c4a49813";
+
+  var cityName = "atlanta";
+
+  var queryURL =
+    "https://api.openweathermap.org/data/2.5/weather?q=" +
+    cityName +
+    "&appid=" +
+    apiKey;
+
+  // FUNCTION DEFINITIONS
+
+  function getWeather() {
+    $.ajax({
+      url: queryURL,
+      method: "GET",
+    }).then(function (response) {
+      console.log(response);
+    });
+  }
+  // FUNCTION CALLS
+  getWeather();
+  // EVENT LISTENERS
+});
