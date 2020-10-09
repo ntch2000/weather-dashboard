cb6196545b6a3061374d0b94994dae9520a42bf2
Neil Gandhi <65824883+ntch2000@users.noreply.github.com>
2020-10-08 22:44:23 -0400

    Completed code to populate 5-day forcast information

--- script.js
+++ script.js
@@ -56,7 +56,7 @@ $(document).ready(function () {
       url: forecastURL,
       method: "GET",
     }).then(function (response) {
-      console.log(response);
+      //console.log(response);
 
       icon = response.current.weather[0].icon;
       // converts the unix UTC datetime to the current date
@@ -67,12 +67,12 @@ $(document).ready(function () {
       windSpeed = response.current.wind_speed;
       uvIndex = response.current.uvi;
 
-      for (var i = 0; i < response.daily.length; i++) {
-        var date = new Date(response.daily[i].dt * 1000).toLocaleDateString();
+      //   for (var i = 0; i < response.daily.length; i++) {
+      //     var date = new Date(response.daily[i].dt * 1000).toLocaleDateString();
 
-        console.log("Day 1: " + date);
-      }
-      populateWeatherData(icon);
+      //     console.log("Day 1: " + date);
+      //  }
+      populateWeatherData(icon, response);
     });
   }
 
@@ -88,28 +88,76 @@ $(document).ready(function () {
       url: queryUrl,
       method: "GET",
     }).then(function (response) {
-      console.log(response);
+      //console.log(response);
 
-      console.log(icon);
+      //console.log(icon);
       cityLon = response.coord.lon;
       cityLat = response.coord.lat;
       currentDate = new Date(response.dt * 1000).toLocaleDateString();
       //console.log(cityLon, cityLat);
-      $("#city-name").text(cityName + " (" + currentDate + ")");
+      $("#city-name").text(city + " (" + currentDate + ")");
 
       getWeatherForecast(cityLat, cityLon);
     });
   }
 
-  function populateWeatherData(weatherIcon) {
+  function populateWeatherData(weatherIcon, weatherObj) {
     var cityWeatherUrl =
       "http://openweathermap.org/img/wn/" + weatherIcon + ".png";
 
+    console.log(weatherObj);
     $("#weatherIcon").attr("src", cityWeatherUrl);
     $("#city-temp").text("Temperature: " + temperature + "\xB0F");
     $("#city-humidity").text("Humidity: " + humidity + "%");
     $("#city-wind").text("Wind Speed: " + windSpeed + " MPH");
     $("#city-uvi").text("UV Index: " + uvIndex);
+
+    for (i = 1; i < weatherObj.daily.length - 2; i++) {
+      // set date for each forecast day
+      var dailyDate = new Date(
+        weatherObj.daily[i].dt * 1000
+      ).toLocaleDateString();
+      // set weather icon -- need to add to function call
+      var dailyIcon =
+        "http://openweathermap.org/img/wn/" +
+        weatherObj.daily[i].weather[0].icon +
+        ".png";
+      console.log(dailyIcon);
+      // set temp
+      var dailyTemp = weatherObj.daily[i].temp.day;
+
+      // set humidity
+      var dailyHumidity = weatherObj.daily[i].humidity;
+
+      createForecastCards(dailyDate, dailyIcon, dailyTemp, dailyHumidity);
+      //console.log(date);
+    }
+  }
+
+  function createForecastCards(date, iconUrl, temp, humidity) {
+    var forecastDiv = $("<div>").attr(
+      "class",
+      "card text-white bg-primary mb-3 p-2"
+    );
+    var forecastHeader = $("<h5>").attr("class", "card-title").text(date);
+
+    var forecastIcon = $("<img>").attr({
+      src: iconUrl,
+      class: "forecast-img",
+    });
+    var forecastTemp = $("<p>")
+      .attr("class", "card-text")
+      .text("Temp: " + temp + " \xB0F");
+    var forecastHumidity = $("<p>")
+      .attr("class", "card-text")
+      .text("Humidity: " + humidity + "%");
+    forecastDiv.append(
+      forecastHeader,
+      forecastIcon,
+      forecastTemp,
+      forecastHumidity
+    );
+    $(".card-deck").append(forecastDiv);
   }
   // FUNCTION CALLS
 
@@ -117,12 +165,14 @@ $(document).ready(function () {
 
   $("#search").on("click", function (event) {
     event.preventDefault();
+    $(".card-deck").empty();
     searchCity();
   });
 
   // listens for any clicks on the city buttons
   $(document).on("click", ".cityBtn", function () {
     console.log($(this).text());
+    $(".card-deck").empty();
     getCityLatLon($(this).text());
   });
 });
